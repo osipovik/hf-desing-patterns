7c3aab091adbe8c47eba2ece1235b6b20f81474b
osipov <osipovik89@gmail.com>
2017-01-07 23:18:15 +0300

    Conditional lock singleton example

---
+++ src/main/java/oik/designpatterns/singleton/cndlock/ChocolateBoiler.java
@@ -0,0 +1,55 @@
+package oik.designpatterns.singleton.cndlock;
+
+/**
+ * Conditional lock example
+ */
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private volatile static ChocolateBoiler instance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (instance == null) {
+            synchronized (ChocolateBoiler.class) {
+                if (instance == null) {
+                    instance = new ChocolateBoiler();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
